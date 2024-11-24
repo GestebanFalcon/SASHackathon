429729072a1addb6ff3ad05d61b968947d9922cb
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-11-23 22:05:36 -0500

    fixed it for real this time

--- src/app/(site)/auth/login/page.tsx
+++ src/app/(site)/auth/login/page.tsx
@@ -13,12 +13,12 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ [
 
         try {
             await signIn("credentials", formData);
-            redirect("/");
+
         } catch (error) {
             console.error(error);
             redirect("/auth/login?error=incorrect")
         }
-
+        redirect("/");
     }
 
     return (
